b38444afe59ae77964e86c802263f668ff16bc46
Chilypepper <peter.a.edin@gmail.com>
2015-07-02 15:42:02 -0400

    finalized launch files for cameras pg.launch does all the work

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -125,6 +125,7 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unsp
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   INCLUDE("/home/peter/brokenGlasses/build/gtest/cmake_install.cmake")
+  INCLUDE("/home/peter/brokenGlasses/build/camera_launches/cmake_install.cmake")
   INCLUDE("/home/peter/brokenGlasses/build/image_pipeline/image_pipeline/cmake_install.cmake")
   INCLUDE("/home/peter/brokenGlasses/build/image_pipeline/camera_calibration/cmake_install.cmake")
   INCLUDE("/home/peter/brokenGlasses/build/image_testing/cmake_install.cmake")
