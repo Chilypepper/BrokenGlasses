7b6520ecef78d370f315143aa53edb90515587f7
chilypepper <chilypepper@github.com>
2015-08-05 18:28:10 -0400

    Added rectangle center-tracker

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/peter/brokenGlasses/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/peter/brokenGlasses/devel;/opt/ros/jade".split(';') if "/home/peter/brokenGlasses/devel;/opt/ros/jade" != "" else []
+underlay_workspaces = "/opt/ros/jade".split(';') if "/opt/ros/jade" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,12 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/peter/brokenGlasses/devel/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/home/peter/brokenGlasses/devel/lib:/opt/ros/jade/lib"
-export PKG_CONFIG_PATH="/home/peter/brokenGlasses/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/home/peter/brokenGlasses/devel/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/peter/brokenGlasses/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/peter/brokenGlasses/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/peter/brokenGlasses/devel/lib:/home/peter/brokenGlasses/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/peter/brokenGlasses/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/peter/brokenGlasses/devel/lib/pkgconfig:/home/peter/brokenGlasses/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/peter/brokenGlasses/build"
+export PYTHONPATH="/home/peter/brokenGlasses/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/peter/brokenGlasses/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/peter/brokenGlasses/src:$ROS_PACKAGE_PATH"
