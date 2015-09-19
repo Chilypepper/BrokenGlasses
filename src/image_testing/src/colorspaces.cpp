74b41ae3c7d0ec3e0067e24dff16e98a5e3fdb89
chilypepper <chilypepper@github.com>
2015-09-19 18:48:20 -0400

    HSC Color works really well now

--- src/image_testing/src/colorspaces.cpp
+++ src/image_testing/src/colorspaces.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <ctime>
 
+#define ptInfo
+
 using namespace cv;
 using namespace std;
 
@@ -74,18 +76,90 @@ public:
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-        Scalar lower_yellow = Scalar(0,80,80);
-        Scalar upper_yellow = Scalar(100,200,200);
-        Mat hsv(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
 
+
+        Mat hsv(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
+        Mat holder = cv_ptr->image;
         cvtColor(cv_ptr->image, hsv, COLOR_BGR2HSV);
 
-        /*Mat mask;
-        inRange(hsv,lower_yellow,upper_yellow,mask);
+        Mat mask;
+        //inRange(hsv,lower_yellow,upper_yellow,mask);
 
         bitwise_and(cv_ptr->image, cv_ptr->image, cv_ptr->image, mask);
-        */
+
          cv_ptr->image = hsv;
+        //**************************************************************
+        //**************************************************************
+        //**************************************************************
+        int centPointx = cv_ptr->image.cols/2;
+        int centPointy = cv_ptr->image.rows/2;
+        int columns = cv_ptr->image.cols;
+        int rows = cv_ptr->image.rows;
+        circle(cv_ptr->image,Point(columns/2,rows/2),2,colorScalar_BLUE,1);
+        int outBlue = cv_ptr->image.at<Vec3b>(Point(centPointx,centPointy))[0];
+        int outGreen = cv_ptr->image.at<Vec3b>(Point(centPointx,centPointy))[1];
+        int outRed = cv_ptr->image.at<Vec3b>(Point(centPointx,centPointy))[2];
+#ifdef ptInfo
+      ROS_INFO("B: %i G: %i R: %i",outBlue,outGreen,outRed);
+#endif
+        int pointx = 0;
+        int pointy = 0;
+        //BGR
+        for(double cols = cv_ptr->image.cols; pointx < cols; pointx+=cols/accuracy){
+            pointy=0;
+            for(double rows = cv_ptr->image.rows; pointy < rows; pointy+=rows/accuracy){
+                if(cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[0] > 20  &&
+                   cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[0] < 38 &&
+                   cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[1] > 170  &&
+                   cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[1] < 210 &&
+                   cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[2] > 90 &&
+                   cv_ptr->image.at<Vec3b>(Point(pointx,pointy))[2] < 200){
+#ifdef findPoint
+                    circle(cv_ptr->image,Point(pointx,pointy),0,colorScalar_BLUE,1);
+#endif
+                    listX.push_back(pointx);
+                    listY.push_back(pointy);
+                }
+            }
+        }
+        int largestX = 0;
+        int largestY = 0;
+        int smallestX = cv_ptr->image.cols;
+        int smallestY = cv_ptr->image.rows;
+
+        int currX=0;
+        int currY=0; //#india
+        int xTotal=0;
+        int yTotal=0;
+        int listSize = listX.size();
+        cv_ptr->image = holder;
+        circle(cv_ptr->image,Point(columns/2,rows/2),2,colorScalar_BLUE,1);
+
+        if(listSize > 20){
+            while(listX.size()>0){
+                currX=listX.back();
+                currY=listY.back();
+
+                xTotal += currX;
+                yTotal += currY;
+
+
+                listX.pop_back();
+                listY.pop_back();
+
+            }
+            int xCentPt = xTotal / listSize;
+            int yCentPt = yTotal / listSize;
+            circle(cv_ptr->image,Point(xCentPt,yCentPt),3,colorScalar_GREEN,1);
+            circle(cv_ptr->image,Point(xCentPt,yCentPt),2,colorScalar_RED,1);
+            line(cv_ptr->image,Point(10+118,10+27),Point(xCentPt,yCentPt),colorScalar_RED,2);
+            Mat rect = cv_ptr->image(Rect(10,10,118,27));
+            Mat color(rect.size(), CV_8UC3, Scalar(0,0,255));
+            double alpha = 0.85;
+            addWeighted(color,alpha,rect,1.0-alpha,0.0,rect);
+            putText(rect,"Target Located",Point(5,17),2,0.45,Scalar(255,255,255),1);
+
+        }
         image_pub.publish(cv_ptr->toImageMsg());
     }
 };
