d56cca35239cc9ac36a56ec9e9b4d14a81e5d73a
chilypepper <chilypepper@github.com>
2015-09-21 17:50:22 -0400

    Changed circles to use opencv algorithms

--- src/image_testing/src/circle_finder.cpp
+++ src/image_testing/src/circle_finder.cpp
@@ -25,52 +25,6 @@ vector<Point> pointList2;
 
 bool foundEdge=false;
 
-int colorDifference(cv_bridge::CvImagePtr cv_ptr, int p1x, int p1y, int p2x, int p2y){
-  int p1r = (int)cv_ptr->image.at<Vec3b>(Point(p1x,p1y))[0];
-  int p1g = (int)cv_ptr->image.at<Vec3b>(Point(p1x,p1y))[1];
-  int p1b = (int)cv_ptr->image.at<Vec3b>(Point(p1x,p1y))[2];
-
-  int p2r = (int)cv_ptr->image.at<Vec3b>(Point(p2x,p2y))[0];
-  int p2g = (int)cv_ptr->image.at<Vec3b>(Point(p2x,p2y))[1];
-  int p2b = (int)cv_ptr->image.at<Vec3b>(Point(p2x,p2y))[2];
-
-  p1r = pow(p1r - p2r,2.0);
-  p1g = pow(p1g - p2g,2.0);
-  p1b = pow(p1b - p2b,2.0);
-  return sqrt(p1r+p1g+p1b);
-
-}
-
-Point findCenter(cv_bridge::CvImagePtr cv_ptr, Point A, Point B, Point C){
-  Point center;
-    float yDelta_a = B.y - A.y;
-    float xDelta_a = B.x - A.x;
-    float yDelta_b = C.y - B.y;
-    float xDelta_b = C.x - B.x;
-
-    float aSlope = yDelta_a/xDelta_a;
-    float bSlope = yDelta_b/xDelta_b;  
-
-    center.x = cv_ptr->image.cols;
-    center.y = cv_ptr->image.rows;
-
-    center.x = (aSlope*bSlope*(A.y - C.y) + bSlope*(A.x + B.x)- aSlope*(B.x+C.x) )/(2* (bSlope-aSlope) );
-    if(aSlope < .01){
-      center.y = -1 * (center.x - (B.x + C.x) / 2) / bSlope +  (B.y + C.y) / 2;
-    }
-    else{
-      center.y = -1*(center.x - (A.x+B.x)/2)/aSlope +  (A.y+B.y)/2;
-    }
-
-  return center;
-}
-
-int findDistance(int p1x, int p1y, int p2x, int p2y){
-  double dx = abs(p1x - p2x);
-  double dy = abs(p1y - p2y);
-  return sqrt(pow(dx,2.0)+pow(dy,2.0));
-}
-
 class ImageConverter
 {
   ros::NodeHandle nh;
@@ -82,7 +36,7 @@ public:
   ImageConverter()
     : it(nh)
   {
-    image_sub = it.subscribe("/camera/image_rect_color", 1, 
+    image_sub = it.subscribe("/camera/image_mono", 1,
       &ImageConverter::imageCb, this);
     image_pub = it.advertise("/camera/output_video", 1);
   }
@@ -94,82 +48,31 @@ public:
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
     }
     catch (cv_bridge::Exception& e)
     {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-
+    Mat im_gray;
+    //cvtColor( cv_ptr->image, im_gray, CV_BGR2GRAY );
+    GaussianBlur(cv_ptr->image, cv_ptr->image, Size(9, 9), 2, 2 );
+    vector<Vec3f> circles;
     //###############Add code between here############################
-  int centerX = cv_ptr->image.cols/2;
-  int centerY = cv_ptr->image.rows/2;
-  int currX = centerX;
-  int currY = centerY;
-  edgeCount = 0;
-  foundEdge=false;
-  
-  while(currX < cv_ptr->image.cols && !foundEdge){
-    if(colorDifference(cv_ptr,currX,currY,currX+5,currY) > 200){
-      pointList.push_back(Point(currX+5,currY));
-      foundEdge=true;
-      edgeCount++;
-    }
-    currX++;
-  }
-
-  foundEdge=false;
-  currX=centerX;
-  while(currX > 5 && !foundEdge){
-    if(colorDifference(cv_ptr,currX,currY,currX-5,currY) > 200){
-      pointList.push_back(Point(currX-5,currY));
-      foundEdge=true;
-      edgeCount++;
-    }
-    currX--;
-  }
-
-  foundEdge=false;
-  currX = centerX;
-  while(currY < cv_ptr->image.rows - 5 && !foundEdge){
-    if(colorDifference(cv_ptr,currX,currY,currX,currY+5) > 200){
-      pointList.push_back(Point(currX,currY+5));
-      foundEdge=true;
-      edgeCount++;
-    }
-    currY++;
-  }
-  
-  while(pointList.size() > 0){
-    Point individual = pointList.back();
-    int x = individual.x;
-    int y = individual.y;
-    circle(cv_ptr->image,Point(x,y),0,colorScalar_RED,1);
-    pointList2.push_back(individual);
-    pointList.pop_back();
-  }
-  if(edgeCount == 3){
-    Point p1 = pointList2.back();
-    pointList2.pop_back();
-    Point p2 = pointList2.back();
-    pointList2.pop_back();
-    Point p3 = pointList2.back();
-    pointList2.pop_back();
-    Point centre = findCenter(cv_ptr,p1,p2,p3);
-    double radius = findDistance(p1.x,p1.y,centre.x,centre.y);
-    radius = abs(radius);
-    cout<<radius<<endl;
-    cout<<centre<<endl;
-    circle(cv_ptr->image,centre,radius,colorScalar_RED,1);
-    cout<<"Found circle, Drawing it!"<<endl;
-    circle(cv_ptr->image,Point(centerX,centerY),4,colorScalar_RED,3);
-  }
-  else{
-    cout<<edgeCount<<endl;
-    cout<<"No circle found!"<<endl;
-  }
+    HoughCircles( cv_ptr->image, circles, CV_HOUGH_GRADIENT, 1, cv_ptr->image.rows/20, 200, 100, 0, 0 );
     //########## AND HERE #######################
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( cv_ptr->image, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( cv_ptr->image, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      ROS_INFO("Made circle!");
+    }
+    cv_ptr->image = im_gray;
     image_pub.publish(cv_ptr->toImageMsg());
   }
 };
@@ -181,3 +84,93 @@ int main(int argc, char** argv)
   ros::spin();
   return 0;
 }
+
+/*
+ * #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <ctime>
+
+using namespace cv;
+using namespace std;
+
+const int testPointX=200;
+const int testPointY=100;
+
+const int accuracy = 200;
+
+int edgeCount= 0;
+//BGR
+Scalar colorScalar_RED = Scalar(0,0,255);
+Scalar colorScalar = Scalar(0,255,0);
+
+vector<Point> pointList;
+vector<Point> pointList2;
+
+bool foundEdge=false;
+
+class ImageConverter
+{
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it;
+  image_transport::Subscriber image_sub;
+  image_transport::Publisher image_pub;
+
+public:
+  ImageConverter()
+    : it(nh)
+  {
+    image_sub = it.subscribe("/camera/image_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub = it.advertise("/camera/output_video", 1);
+  }
+
+  ~ImageConverter()
+  {}
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    Mat im_gray;
+    //cvtColor( cv_ptr->image, im_gray, CV_BGR2GRAY );
+    GaussianBlur(im_gray, im_gray, Size(9, 9), 2, 2 );
+    vector<Vec3f> circles;
+    //###############Add code between here############################
+    HoughCircles( im_gray, circles, CV_HOUGH_GRADIENT, 1, im_gray.rows/20, 200, 100, 0, 0 );
+    //########## AND HERE #######################
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( cv_ptr->image, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( cv_ptr->image, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      ROS_INFO("Made circle!");
+    }
+    cv_ptr->image = im_gray;
+    image_pub.publish(cv_ptr->toImageMsg());
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
+}
+
+ */
