b2fb55c8e70b7fb78efccf91538c5c50cd973ab0
chilypepper <chilypepper@github.com>
2015-10-02 12:20:56 -0400

    fixed a method with no body

--- src/image_testing/src/draw_gui.cpp
+++ src/image_testing/src/draw_gui.cpp
@@ -159,7 +159,8 @@ public:
         detectedCircleR = circArr.radius;
     }
     void updateColorInfo(const point_message::statsMsg objectColorInfo){
-
+        objectColorX = objectColorInfo.xCent;
+        objectColorY = objectColorInfo.yCent;
     }
 };
 
