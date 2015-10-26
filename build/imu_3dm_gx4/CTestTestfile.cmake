60a27acc9c9660336ece20c0c9c71f3b91946798
chilypepper <chilypepper@github.com>
2015-10-25 21:48:10 -0400

    Added imu files to repo

---
+++ build/imu_3dm_gx4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peter/brokenGlasses/src/imu_3dm_gx4
+# Build directory: /home/peter/brokenGlasses/build/imu_3dm_gx4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/orientation/CMakeFiles/quaternion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/quaternion.dir/src/quaternion.cpp.o"
+  "/home/peter/brokenGlasses/devel/lib/learn_imu/quaternion.pdb"
+  "/home/peter/brokenGlasses/devel/lib/learn_imu/quaternion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/quaternion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
