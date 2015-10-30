9680046a1023cce0c84d3b2a6d84e7918e3de10e
chilypepper <chilypepper@github.com>
2015-10-30 15:44:26 -0400

    Can seperate reds

---
+++ build/imu/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peter/brokenGlasses/src/imu
+# Build directory: /home/peter/brokenGlasses/build/imu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
