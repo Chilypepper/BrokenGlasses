dd6a4467ebaf7fe590e222e344e27b356315ce88
chilypepper <chilypepper@github.com>
2015-10-30 14:36:01 -0400

    Added heading node, framework done

--- src/image_testing/src/heading_marker.cpp
+++ src/image_testing/src/heading_marker.cpp
@@ -47,13 +47,41 @@ public:
         int satLower = 150;
         int valUpper = 240;
         int valLower = 150;
+
+        bool hasHue = false;
+        bool hasSat = false;
+        bool hasVal = false;
+        Vec3b white = {255,255,255};
+
         Mat blacknwhite = Mat::zeros(marker.rows,marker.cols,CV_64F);
 
         for(int row = 0; row < blacknwhite.rows; row++){
             for(int col = 0; col < blacknwhite.cols; col++){
                 Vec3b hsvValues = marker.at<Vec3b>(Point(row,col));
+                int hue = hsvValues[0];
+                int saturation = hsvValues[1];
+                int brightness = hsvValues[2];
+                //red is special case given its hue values
+                if(hue > redUpper || hue < redLower){
+                    /*
+                     * checks HSV conditions and determines if all are true
+                     */
+                    hasHue = true;
+                    if(saturation < satUpper && saturation < satLower){
+                        hasSat = true;
+                        if(brightness < valUpper && brightness > valLower){
+                            hasVal = true;
+                        }
+                    }
+                }
+                //assumes all true
+                if(hasHue && hasSat && hasVal){
+                    blacknwhite
+                }
+
             }
         }
+
 
 
 
