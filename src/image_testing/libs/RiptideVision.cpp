ddfb7074db49c5823d89fe049dd4973412c5f5e1
edinpeter <edin.3@osu.edu>
2016-03-01 17:57:23 -0500

    Added orientation function

--- src/image_testing/libs/RiptideVision.cpp
+++ src/image_testing/libs/RiptideVision.cpp
@@ -8,6 +8,10 @@ struct colorPoint{
   int x;
   int y;
 };
+struct linePoint{
+  colorPoint top;
+  colorPoint bot;
+};
 
 Mat RiptideVision::seperateColors(Mat src, vector<int> colors)
 {
@@ -49,7 +53,51 @@ void RiptideVision::colorAverage(Mat src, vector<int> colors, colorPoint &averag
   averagePoint.x = jSum / count;
   averagePoint.y = iSum / count;
 }
+void RiptideVision::orientation(Mat src, vector<int> colors, colorPoint averagePoint, linePoint &pair){
+  Mat M = seperateColors(src,colors); 
 
+  averagePoint.x = 0;
+  averagePoint.y = 0;
+
+  long long int iSumTop = 0;
+  long long int jSumTop = 0;
+
+  long long int iSumBot = 0;
+  long long int jSumBot = 0;
+
+  int count = 0;
+
+  //**Top half
+  for(int i = 0; i < M.cols; i += 2){
+    for(int j = 0; j < averagePoint.y; j += 2){
+      //cout << i << " " << j << endl;
+      if(M.at<uchar>(i,j) > 0){
+        iSumTop += i;
+        jSumTop += j;
+        count++;
+      }
+    }
+  }
+  pair.top.x = iSumTop / count;
+  pair.top.y = jSumTop / count;
+
+  count = 0;
+
+  //** Bottom half
+  for(int i = 0; i < M.cols; i += 2){
+    for(int j = averagePoint.y; j < M.rows; j += 2){
+      //cout << i << " " << j << endl;
+      if(M.at<uchar>(i,j) > 0){
+        iSumBot += i;
+        jSumBot += j;
+        count++;
+      }
+    }
+  }
+  pair.bot.x = iSumBot / count;
+  pair.bot.y = jSumBot / count;
+
+}
 
 int main(){
 
--- src/image_testing/libs/RiptideVision.h
+++ src/image_testing/libs/RiptideVision.h
@@ -6,7 +6,12 @@ class RiptideVision{
 			int x;
 			int y;
 		};
+		struct linePoint{
+		  colorPoint top;
+		  colorPoint bot;
+		};
 		static cv::Mat seperateColors(cv::Mat, std::vector<int>);
 		static void colorAverage(cv::Mat, std::vector<int>, colorPoint&);
+		static void orientation(cv::Mat, std::vector<int>, colorPoint, linePoint&);
 };
 
